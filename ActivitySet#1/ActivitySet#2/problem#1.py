a48a852d05ad98c60101bba29892bd71dc9972e1
Ankitvats <95743251+Ankit07vts@users.noreply.github.com>
2022-05-21 15:13:08 +0000

    .

--- ActivitySet#1/ActivitySet#2/problem#1.py
+++ ActivitySet#1/ActivitySet#2/problem#1.py
@@ -1,6 +1,6 @@
-num1 = input("enter 1st number")
-num2 = input("enter 2nd number")
+num1 = float(input("enter 1st number"))
+num2 = float(input("enter 2nd number"))
 
-sum =float(num1)+float(num2)
+sum = num1 + num2
 
 print("the sum of {0} and {1} is {2}".format(num1,num2,sum))
--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -2,11 +2,18 @@
 
 
 def computepay(h, r):
-    pass  # ...
+    if h>40:
+       
+        rate1=(r*1.5)*(h-40)
+        pay=(r*40)+rate1
+    else :
+        pay=r*h
+        
+    return pay
 
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
+hrs = input("Enter Hours:")
+h=float(hrs)
+rate=input("enter the rate")
+r=float(rate)
+p = computepay(h,r)
 print("Pay", p)
