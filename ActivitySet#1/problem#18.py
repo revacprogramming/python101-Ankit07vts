441edd4252896b1b1396b9b535cf8bea7771e67b
Ankitvats <95743251+Ankit07vts@users.noreply.github.com>
2022-05-31 10:04:40 +0000

    .

---
+++ ActivitySet#1/problem#18.py
@@ -0,0 +1,16 @@
+fname = input("Enter file name: ")
+
+fh = open(fname)
+count = 0
+for line in fh:
+    
+    line=line.rstrip()
+    if not line.startswith("From") or line.startswith(
+"From:"):
+       continue
+    words = line.split()
+    print(words[1])
+    count=count+1
+    
+
+print("There were", count, "lines in the file with From as the first word")
